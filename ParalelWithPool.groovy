410eacdcd360019bcc57689129dc62c830c2089d
Yakim H. Rachev <jakimcho@gmail.com>
2019-06-09 16:38:06 +0300

    Improved ParallelWithPool script

--- ParalelWithPool.groovy
+++ ParalelWithPool.groovy
@@ -1,4 +1,5 @@
 def branches = [:]
+// List with existing job names that need to run in parallel
 String[] jobs = [
   "Project Package Job",
   "Project Test Deploy Job",
@@ -10,38 +11,40 @@ String[] jobs = [
   "Project Realease Job"
 ]
 
-// setup a latch
+// Define the pool of tickets and max tickets number
 MAX_CONCURRENT = 3
+MAX_POLL_TIMEOUT = 1
+TIME_UNITS = java.util.concurrent.TimeUnit.SECONDS
 ticketsPool = new java.util.concurrent.LinkedBlockingDeque(MAX_CONCURRENT)
-// put a number of items into the queue to allow that number of branches to run
+
+// put a number of tickets into the queue to allow that number of branches to run
 for (int i=0; i<MAX_CONCURRENT; i++) {
-    ticketsPool.offer("$i")
+  ticketsPool.offer("ticket $i")
 }
 
-for (int i=0; i < jobs.length; i++) {
-    def jobName = jobs[i]
-    branches[jobName] = {
-        def ticket = null
-        // this will not allow proceeding while there are no tickets in the queue
-        waitUntil {
-            ticket = ticketsPool.pollFirst();
-            return ticket != null;
-        }
-        try {
-            echo "Hello from $jobName"
-            // def timeout = (Math.random() * 10 + 1).intValue()
-            // echo "Sleep for $timeout seconds"
-            // sleep time: timeout, unit: 'SECONDS'
-            build jobName
-            echo "Goodbye from $jobName"
-        }
-        finally {
-           // return the ticket back into the queue to allow others to proceed
-            ticketsPool.offer(ticket)
-        }
+for (int i = 0; i < jobs.length; i++) {
+  def job = jobs[i];
+  branches[job] = {
+    def ticket = null
+    // this will not allow proceeding while there are no tickets in the queue
+    waitUntil {
+      ticket = ticketsPool.pollFirst( MAX_POLL_TIMEOUT, TIME_UNITS );
+      return ticket != null;
     }
+    try {
+      echo "Hello from $job that got \"$ticket\" ticket to join parallel run"
+      build job
+      echo "Goodbye from $job and ticket \"$ticket\" will be returned"
+    }
+    finally {
+      // return the ticket back into the queue to allow others to proceed
+      ticketsPool.offer(ticket)
+    }
+  }
 }
 
 timestamps {
+  stage("Regression testing"){
     parallel branches
+  }
 }
--- ReadMe.md
+++ ReadMe.md
@@ -3,3 +3,25 @@
 ## Purpose
 
 Here I'll collect all pipeline strategies that would be usefull for my and my projects
+
+## TODO:
+
+Currently when run ParallelWithPool scripted pipeline, there is increaseing waits per branch that comes from jenkins.
+
+```
+16:20:00  Will try again after 0.25 sec
+16:20:00  Will try again after 0.25 sec
+..............
+16:20:15  Will try again after 0.3 sec
+.......
+16:20:15  Will try again after 0.3 sec
+..........
+16:20:22  Will try again after 0.36 sec
+16:20:22  Will try again after 0.36 sec
+............
+16:20:25  Will try again after 0.43 sec
+...........
+16:20:29  Will try again after 0.51 sec
+```
+
+Need to find a way to set this waits to a static duration
